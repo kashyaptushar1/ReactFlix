ee7d9ed44fc9c8d3bb952b2bc625c57ae96a9d40
tushar <tusharkashayp9368@gmail.com>
2024-10-28 07:20:38 +0530

    format Horizontal cards

--- src/components/partials/HorizontalCards.jsx
+++ src/components/partials/HorizontalCards.jsx
@@ -1,28 +1,34 @@
-import React from 'react'
+import React from "react";
 
-
-function HorizontalCards({data}) {
+function HorizontalCards({ data }) {
   return (
-    
-           
-
-            <div className='w-[100%] flex overflow-y-hidden mb-3 ' >
-                {data.map((d,i)=>(
-                    <div key={i} className='min-w-[15%]  ml-5 mb-5 bg-zinc-900 overflow-auto'>
-                        <img className=' w-full h-[55%] object-cover' src={`https://image.tmdb.org/t/p/original/${d.backdrop_path || d.profile_path}`} alt="" />
-                      <div className='text-white p-3 h-[45%]' >
-                      <h1 className=' text-xl font-semibold text-[#6556CD] ' > {d.title || d.name || d.original_name || d.original_title}</h1>
-                       <p className=' mb-5 pb-5 ' >
-                        {d.overview.slice(0, 50)}...
-                        <span className='text-blue-400' >more</span>
-                       </p>
-                      </div>
-                    </div>
-                ))}
-            </div>
-      
-    
-  )
+    <div className="w-[100%] flex overflow-y-hidden mb-3 ">
+      {data.map((d, i) => (
+        <div
+          key={i}
+          className="min-w-[15%]  ml-5 mb-5 bg-zinc-900 overflow-auto"
+        >
+          <img
+            className=" w-full h-[55%] object-cover"
+            src={`https://image.tmdb.org/t/p/original/${
+              d.backdrop_path || d.profile_path
+            }`}
+            alt=""
+          />
+          <div className="text-white p-3 h-[45%]">
+            <h1 className=" text-xl font-semibold text-[#6556CD] ">
+              {" "}
+              {d.title || d.name || d.original_name || d.original_title}
+            </h1>
+            <p className=" mb-5 pb-5 ">
+              {d.overview.slice(0, 50)}...
+              <span className="text-blue-400">more</span>
+            </p>
+          </div>
+        </div>
+      ))}
+    </div>
+  );
 }
 
-export default HorizontalCards
+export default HorizontalCards;
