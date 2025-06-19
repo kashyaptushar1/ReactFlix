97dcc84b05eccf118a3ba858e1652d5afbc94fec
tushar <tusharkashayp9368@gmail.com>
2025-06-19 12:41:23 +0530

    last

--- src/utils/axios.jsx
+++ src/utils/axios.jsx
@@ -1,9 +1,13 @@
 import axios from "axios";
+
+
+// Create Axios instance using env variables
 const instance = axios.create({
-    baseURL:"https://api.themoviedb.org/3/",
-    headers: {
-        accept: 'application/json',
-        Authorization: 'Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiJmOGYxMGI4ZTYwNjc3ODk2YzlkN2QyNWVkMmU5ZmQ3MSIsIm5iZiI6MTcyOTk1NjE0My4yODMxNTcsInN1YiI6IjY3MGEyMTE2M2JiNDU1N2M2NjlhZjdhYyIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.xYBOE2N11xMdKwQdWhr34r0RsKKp85NrXoX0zi0ayts'
-      }
+  baseURL:  import.meta.env.VITE_API,
+  headers: {
+    accept: "application/json",
+    Authorization: `Bearer ${ import.meta.env.VITE_TMDB_TOKEN}` // ✅ Use template string
+  }
 });
+
 export default instance;
